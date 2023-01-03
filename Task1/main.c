14ffe9edf77e599664ccfd8a6652ba75d7b837dc
marijacetkovic <marijacetkovic03@gmail.com>
2023-01-03 22:26:04 +0100

    Added multiple passengers

--- Task1/main.c
+++ Task1/main.c
@@ -1,62 +1,105 @@
+//elevator controller program in C
+/* program simulates the behaviour of the elevator
+ user inputs desired floor and elevator travels to that floor
+ user can exit by typing EXIT
+ user can stop on any floor on the way to the desired one by typing YES
+ if user leaves before reaching desired floor, and wants to ride again he types in CALL
+ and elevator gets called from the desired floor
+
+ elevator can also accept different number of passengers (up to 4)
+ if the number of passengers is bigger than 4 at any point, lift will as many as can fit (4)
+ passengers can enter or exit at any floor 
+*/
+
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-//function declaration
+//function declaration on beginning of the program
+//so the compiler knows that im going to use them
+//functions are defined below main
 int sFloorToInt(char sFloor[]);
-const char* printLiftStatus(int dFloorNum);
+const char* intToFloor(int dFloorNum);
+void printWelcome();
 void printButtons();
+void printStatus(int dFloorInt);
+void peopleStatus(int dPeople);
 
+
+
+
+//main
 int main(){
 
 
 	//variable declaration
+
+	//default floor, starting G
 	char sFloorDefault[5] = "G";
-	char sFloorChosen[5] = "G";
+
+	//floor the user chooses
+	char sFloorChosen[5] ;
+
+	//takes input for stopping the lift
 	char sStopping[5];
+
+	//this variable stores most recent floor where user stopped the lift
 	char sFloorNewDefault[5] = "G";
+
+	//takes user input
 	char sUserInput[5];
+
+	//int variables of the floors
 	int dFloorDefaultInt=sFloorToInt(sFloorDefault);
 	int dFloorChosenInt;
 	int dFloorNewDefaultInt;
+	int dPeople;
+	int dPeopleNew;
+	char sPeople[4];
 
-	// // while (1){
-	// 	if(floorToInt(dFloor)==10){
-	// 		printf("There is no such floor.\n");
-	// 		break;
-	// 	}
-	// 	else{
-	// 		printLiftStatus(dFloor);
-	// 		break;
-	// 	}
-	// // }
-	printf("\nHi there! Where am I taking you?\n");
-	printf("If you want to stop the lift anywhere, type in YES.\n");
-	printf("If you want to exit the lift, type in EXIT.\n");
-	printf("You are now on floor G.\n");
+
+
+	//print welcome message
+	printWelcome();
+
+
+
 	//mainwhile
 	while(1){
 
 		
-		//take user input to change floors
+		//show buttons of the elevator
 		printf("\n");
 		printButtons();
-		//fgets(dFloorChosen,3,stdin);
+
+		//take user input to change floors using scanf
 		scanf("%s", sFloorChosen);
 
+		printf("How many people are entering?\n");
+		scanf("%d",&dPeople);
+		if(dPeople>4){
+			printf("No more than 4 can fit.\n");
+			dPeople=4;
+		}
+
+		//if the user enters EXIT, break the main while loop and end the program
 		if (strncmp(sFloorChosen, "EXIT", 4)==0){
 			printf("Leaving so soon? Bye.\n");
 			break;
 		}//endif
 
+		//copy new default floor to default floor
 		strcpy(sFloorDefault,sFloorNewDefault);
 
 		//change two floor variables to integers
 		dFloorChosenInt=sFloorToInt(sFloorChosen);
 		dFloorDefaultInt=sFloorToInt(sFloorDefault);
-		//dFloorNewDefaultInt=sFloorToInt(sFloorNewDefault);
-
+		
 		
 
 		//check for wrong input
@@ -73,14 +116,33 @@ int main(){
 
 			while(dFloorChosenInt>dFloorDefaultInt){
 				dFloorDefaultInt++;
-				printf("\nYou're currently at floor %s",printLiftStatus(dFloorDefaultInt));
-				
-				strcpy(sFloorNewDefault,printLiftStatus(dFloorDefaultInt));
+				printStatus(dFloorDefaultInt);
+				peopleStatus(dPeople);
+				strcpy(sFloorNewDefault,intToFloor(dFloorDefaultInt));
 				if ( dFloorDefaultInt != dFloorChosenInt ) {
 					printf("\nStopping here?\n");
 					scanf("%s",sStopping);
 					if (strcmp(sStopping,"YES")==0){
+						printf("How many are leaving?\n");
+						scanf("%d", &dPeopleNew);
+						dPeople -= dPeopleNew;
+						peopleStatus(dPeople);
+
+						printf("Are there new people?\n");
+						scanf("%s",sPeople);
+						if (strcmp(sPeople,"YES")==0){
+							printf("How many?\n");
+							scanf("%d",&dPeopleNew);
+							dPeople += dPeopleNew;
+							if (dPeople > 4){
+								printf("Lift can accept up to 4.\n");
+								dPeople = 4;
+								peopleStatus(dPeople);
+							}
+						}
+					if(dPeople == 0){
 					break;
+					}
 					}//endif
 				}
 			}//endwhile
@@ -93,16 +155,37 @@ int main(){
 		}
 
 		//if not travel down
-		else{
+		else if(dFloorChosenInt<dFloorDefaultInt){
 			while(dFloorChosenInt<dFloorDefaultInt){
 				dFloorDefaultInt--;
-				printf("\nYou're currently at floor %s",printLiftStatus(dFloorDefaultInt));
-				strcpy(sFloorNewDefault,printLiftStatus(dFloorDefaultInt));
+				
+				printStatus(dFloorDefaultInt);
+				peopleStatus(dPeople);
+				strcpy(sFloorNewDefault,intToFloor(dFloorDefaultInt));
 				if ( dFloorDefaultInt != dFloorChosenInt ) {
 					printf("\nStopping here?\n");
 					scanf("%s",sStopping);
 					if (strcmp(sStopping,"YES")==0){
+						printf("How many are leaving?\n");
+						scanf("%d", &dPeopleNew);
+						dPeople -= dPeopleNew;
+						peopleStatus(dPeople);
+
+						printf("Are there new people?\n");
+						scanf("%s",sPeople);
+						if (strcmp(sPeople,"YES")==0){
+							printf("How many?\n");
+							scanf("%d",&dPeopleNew);
+							dPeople += dPeopleNew;
+							if (dPeople > 4){
+								printf("Lift can accept up to 4.\n");
+								dPeople = 4;
+								peopleStatus(dPeople);
+							}
+						}
+					if(dPeople == 0){
 					break;
+					}
 					}//endif
 				}
 			}//endwhile
@@ -121,12 +204,14 @@ int main(){
 
 				while(dFloorDefaultInt>dFloorChosenInt){
 					dFloorChosenInt++;
-					printf("Lift at floor %s.\n", printLiftStatus(dFloorChosenInt));
+					printStatus(dFloorChosenInt);
+					
 
 				}
 				while(dFloorDefaultInt<dFloorChosenInt){
 					dFloorChosenInt--;
-					printf("Lift at floor %s.\n", printLiftStatus(dFloorChosenInt));
+					printStatus(dFloorChosenInt);
+					
 
 				}
 			}//endif
@@ -138,20 +223,24 @@ int main(){
 	}//endmainwhile
 
 
-		
-	
-	
-
-
-
 
 	return 0;
 }//endmain
 
+
+
+//function used to change string floor input to int 
+//main logic of the algorithm is based on int comparison so int is needed
 int sFloorToInt(char sFloor[]){
+
+	//declare var
 	int dFloorNum;
+
+	//atoi changes char to int, has 0 return value if cant be changed (char is not a number)
 	dFloorNum = atoi(sFloor);
 
+
+	//if char is not a number change G, B1, B2, B3 respectively
 	if (dFloorNum==0){
 		if(strncmp(sFloor, "G",1)==0){
 		 	 return dFloorNum = 0;
@@ -167,13 +256,16 @@ int sFloorToInt(char sFloor[]){
 			 return dFloorNum = -3;
 		}
 		else{
-			return 10;
+			//if user entered anything else return an error
+			return 10; //used random number say 10 for reference
 		}
 	}
 	if(dFloorNum>5 || dFloorNum<0){
+		//also if user gave any numeric input out of scope of the lift error
 		return 10;
 	}
 	else{
+		//if everythings correct return dFloorNum
 		return dFloorNum;
 	}
 		
@@ -181,15 +273,22 @@ int sFloorToInt(char sFloor[]){
 
 
 
-
-const char* printLiftStatus(int dFloorNum){
+// function to change floor given in int to its string twin
+// used to allow printing correct names of the floors to user 
+// ex. -1 will be used by the algorithm but user gets B1 printed out
+const char* intToFloor(int dFloorNum){
 	printf("\n");
 	static char sFloorString[5];
 
+	//use sprintf to print directly to the string variable sFloorString
+
+	//if our floor is numeric, print that number to the string
 	if(dFloorNum > 0){
 		sprintf(sFloorString,"%d",dFloorNum);
 
 	}
+
+	//if not, print the string name (respecting the number analogy)
 	else if (dFloorNum == 0){
 
 		sprintf(sFloorString,"G");
@@ -206,38 +305,26 @@ const char* printLiftStatus(int dFloorNum){
 
 		sprintf(sFloorString,"B3");
 	}
+
+	//return the string
 	return sFloorString;
 
 }//endfunction
 
 
-// void printLiftStatus(int dFloorNum){
-// 	printf("\n");
+//function to print welcome message
+void printWelcome(){
+	printf("\nHi there! Where am I taking you?\n");
+	printf("If you want to stop the lift anywhere, type in YES.\n");
+	printf("If you want to exit the lift, type in EXIT.\n");
+	printf("You are now on floor G.\n");
+}//endfunction
 
-// 	if(dFloorNum > 0){
-// 		printf("You're at floor %d.\n",dFloorNum);
-// 	}
-// 	else if (dFloorNum == 0){
 
-// 		printf("You're at floor G.\n");
-// 	}
-// 	else if (dFloorNum == -1){
-
-// 		printf("You're at floor B1.\n");
-// 	}
-// 	else if (dFloorNum == -2){
-
-// 		printf("You're at floor B2.\n");
-// 	}
-// 	else if (dFloorNum == -3){
-
-// 		printf("You're at floor B3.\n");
-// 	}
-
-// }//endfunction
-
+//function to print buttons of the elevator
 void printButtons(){
 
+	//print the buttons
 	printf("Select a floor to ride the elevator.\n\n");
 	printf(" \n");
 	printf(" [ 3 ] [ 4 ] [ 5 ]\n");
@@ -245,3 +332,18 @@ void printButtons(){
 	printf("[ B3 ] [ B2 ] [ B1 ]\n");
 
 }//endfunction
+
+//function to print the status of the lift
+//used to simplify main
+void printStatus(int dFloorInt){
+
+	//print the status using intToFloor function to convert int to string floor 
+	printf("Lift at floor %s.\n", intToFloor(dFloorInt));
+}//endfunction
+
+void peopleStatus(int dPeople){
+	printf("There are %d people in the lift.\n", dPeople);
+}
+
+
+
